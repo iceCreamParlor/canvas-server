6bf0dd67a0a6306c556c84b617352c3a39ad6116
iceCreamParlor <heejjeeh@gmail.com>
2019-01-25 20:09:18 +0900

    유저타입 추가

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum user_type: ["normal", "admin"]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
