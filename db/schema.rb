9797f528e7fdab50d9dde0a7d75592ec1f057f6a
iceCreamParlor <heejjeeh@gmail.com>
2019-02-01 11:28:16 +0900

    active_admin 충돌 수정(will_paginate)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_31_053719) do
+ActiveRecord::Schema.define(version: 2019_02_01_021800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,12 @@ ActiveRecord::Schema.define(version: 2019_01_31_053719) do
     t.index ["category_id"], name: "index_paintings_on_category_id"
     t.index ["color_id"], name: "index_paintings_on_color_id"
     t.index ["user_id"], name: "index_paintings_on_user_id"
+  end
+
+  create_table "prices", force: :cascade do |t|
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
