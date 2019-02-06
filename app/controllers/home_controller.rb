ea33699d17851d2d8b298b71608c0764eb825ffc
iceCreamParlor <heejjeeh@gmail.com>
2019-02-06 13:42:34 +0900

    메인 페이지 수정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @paintings1 = Painting.all.sample(10).map{ |p| { 'id' => p.id, 'thumbnail' => p.thumbnail.url } }
+    @paintings2 = Painting.all.select(:thumbnail).sample(10)
+    @paintings3 = Painting.all.select(:thumbnail).sample(10)
+    @magazines = Magazine.all.map{ |p| p.thumbnail }
   end
 end
