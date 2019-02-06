6d97f9829a47aa081246c8e490bd55e63147f6f7
iceCreamParlor <heejjeeh@gmail.com>
2019-02-07 00:10:49 +0900

    링크 컬러 변경

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @paintings1 = Painting.all.sample(10).map{ |p| { 'id' => p.id, 'thumbnail' => p.thumbnail.url } }
+    @paintings1 = Painting.all.map{ |p| { 'id' => p.id, 'thumbnail' => p.thumbnail.url } }
     @paintings2 = Painting.all.select(:thumbnail).sample(10)
     @paintings3 = Painting.all.select(:thumbnail).sample(10)
     @magazines = Magazine.all.map{ |p| p.thumbnail }
