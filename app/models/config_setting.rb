3e9ec43c05a77916fc6e45de34b0680aefb7c070
iceCreamParlor <heejjeeh@gmail.com>
2019-03-25 16:24:39 +0900

    프론트와 백엔드 로직 연결

--- app/models/config_setting.rb
+++ app/models/config_setting.rb
@@ -4,7 +4,7 @@ class ConfigSetting < ApplicationRecord
     def get_web_magazines
       str = self.where(key: "web_magazines").last&.value
       if str
-        WebMagazine.where(id: str.split(",").map { |s| s.to_i })
+        WebMagazine.where(number: str.split(",").map { |s| s.to_i })
       else
         WebMagazine.last(3)
       end
