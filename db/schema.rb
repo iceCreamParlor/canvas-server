1cc75e282ea4664f72df830dbd4354c65e637a7e
iceCreamParlor <heejjeeh@gmail.com>
2019-03-31 16:09:34 +0900

    작가 프로필 페이지 일부 변경

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_050415) do
+ActiveRecord::Schema.define(version: 2019_03_25_025321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -308,7 +308,6 @@ ActiveRecord::Schema.define(version: 2019_03_31_050415) do
     t.string "unconfirmed_email"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "major"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
