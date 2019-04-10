44bb0514650096dac84d8ea5b1c98965b894cb85
iceCreamParlor <heejjeeh@gmail.com>
2019-04-10 20:56:26 +0900

    error fix

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -2,11 +2,11 @@ class MagazinesController < InheritedResources::Base
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
-    # @normal_magazines = Magazine.where(priority: "normal").order("created_at DESC")
-    # @head_magazines = Magazine.where(priority: "head").order("created_at DESC")
-    # @main_magazines = Magazine.where(priority: "main").order("created_at DESC")
-    # @painting_magazines = Magazine.where(magazine_type: "painting") - @head_magazines - @main_magazines
-    # @artist_magazines = Magazine.where(magazine_type: "artist") - @head_magazines - @main_magazines
+    @normal_magazines = Magazine.where(priority: "normal").order("created_at DESC")
+    @head_magazines = Magazine.where(priority: "head").order("created_at DESC")
+    @main_magazines = Magazine.where(priority: "main").order("created_at DESC")
+    @painting_magazines = Magazine.where(magazine_type: "painting") - @head_magazines - @main_magazines
+    @artist_magazines = Magazine.where(magazine_type: "artist") - @head_magazines - @main_magazines
     @paintings = Painting.last(3)
     
     # render layout: 'layouts/customized_layout', template: 'magazines/index_mobile' if @is_mobile
